4d35cfcd9dcd8e0cdf575ac3563b08133f22e080
junki <gofish948@icloud.com>
2026-03-30 23:06:42 +0900

    expo setting

---
+++ app/index.tsx
@@ -0,0 +1,48 @@
+import { Text, View, Alert } from 'react-native';
+import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as AppleAuthentication from 'expo-apple-authentication'; // 애플 로그인 기능 불러오기!
+
+export default function LoginScreen() {
+
+    // 애플 로그인 버튼 핸들
+    const handleAppleLogin = async () => {
+        try {
+            const credential = await AppleAuthentication.signInAsync({
+                requestedScopes: [
+                    AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                    AppleAuthentication.AppleAuthenticationScope.EMAIL,
+                ],
+            });
+            // 로그인이 성공 시 수행
+            console.log('애플 로그인 성공. 정보:', credential);
+            router.push('/home');
+
+        } catch (e: any) {
+            if (e.code === 'ERR_REQUEST_CANCELED') {
+                console.log('유저가 로그인 창을 닫았습니다.');
+            } else {
+                Alert.alert('로그인 실패', '애플 로그인 중 오류가 발생했습니다.');
+                console.error(e);
+            }
+        }
+    };
+
+    return (
+        <SafeAreaView className="flex-1 justify-center items-center bg-red-50 px-5">
+            <View className="items-center mb-16">
+                <Text className="text-4xl font-bold text-red-500 mb-2">Allergy Safe</Text>
+                <Text className="text-gray-600 text-lg">유학생을 위한 맞춤형 식단 안내</Text>
+            </View>
+
+            {/* 애플 로그인 버튼 (수정x) */}
+            <AppleAuthentication.AppleAuthenticationButton
+                buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+                buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+                cornerRadius={12}
+                style={{ width: '100%', height: 50 }}
+                onPress={handleAppleLogin}
+            />
+        </SafeAreaView>
+    );
+}
